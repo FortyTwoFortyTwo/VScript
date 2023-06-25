c49fd0cc91ab784199d15346d33da0cbc68c722a
FortyTwoFortyTwo <connorrowlanduk@gmail.com>
2023-06-25 10:52:32 +0100

    Add vscript.inc to package build

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,8 +4,10 @@ cd build
 # Create package dir
 mkdir -p package/addons/sourcemod/plugins
 mkdir -p package/addons/sourcemod/gamedata
+mkdir -p package/addons/sourcemod/scripting
 
 # Copy all required stuffs to package
 cp -r addons/sourcemod/plugins/vscript.smx package/addons/sourcemod/plugins
 cp -r ../gamedata/vscript.txt package/addons/sourcemod/gamedata
+cp -r ../scripting/include package/addons/sourcemod/scripting
 cp -r ../LICENSE package
